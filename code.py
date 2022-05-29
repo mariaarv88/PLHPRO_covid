758964d0f95bf6a9ad813e552348401fa9a7dd41
Maria Arv <79372611+mariaarv88@users.noreply.github.com>
2022-05-29 18:50:45 +0200

    Update code.py

--- code.py
+++ code.py
@@ -185,8 +185,8 @@ row_spacer_start, row1, row2, row_spacer_end  = st.columns((0.1, 1.0, 6.4, 0.1))
 
 with row1:
     #add here everything you want in first column
-    plot_value = st.selectbox ("Linear regression", list(value_labels.keys()), key = 'value_key') #take all the keys from value_labels dictionary
-    
+    #plot_value = st.selectbox ("Linear regression", list(value_labels.keys()), key = 'value_key') #take all the keys from value_labels dictionary
+    st.markdown("Linear Regression")
     
 # ----------------------------------------- linear regression -----------------------------------------#
 # input arrays
